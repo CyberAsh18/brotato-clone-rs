240f91eac8283c22fc4dadc5a373d2d5c1909801
CyberAsh18 <ashfaaq.riphque@cyberowl.io>
2024-07-19 18:35:48 +0800

    more stuff

--- src/main.rs
+++ src/main.rs
@@ -7,40 +7,46 @@ use macroquad::prelude::*;
 fn conf() -> Conf {
     Conf{
         window_title: "WormGrounds".to_string(),
+        window_width: 640,
+        window_height: 480,
         fullscreen: false,
         window_resizable: false,
         ..Default::default()
     }
 }
 
+
+
 #[macroquad::main(conf)]
 async fn main() {
     info!("Initializing modules");
 
-    //1 layer example here
-    let mut map_design: Vec<Vec<Vec<[i32; 2]>>> =
-        vec![
-            vec![
-                vec![ [1,1], [1,1], [1,1] ,[1,1]],
-                vec![ [1,1], [1,1], [1,1] ,[1,1]],
-                vec![ [1,1], [2,2], [1,1] ,[1,1]],
-                vec![ [1,1], [1,1], [3,3] ,[1,1]],
-                vec![ [1,1], [1,1], [1,1] ,[1,1]],
-                vec![ [1,1], [1,1], [1,1] ,[0,1]],
-            ]
-        ];
     //map
-    let bg_map = output::BgMap::initialize(
-        "assets/background.png",
-        32,
-        map_design.clone()).await;
-    if bg_map.is_none() {return;}
+    let bg_map = output::BgMap::initialize("assets/background_map.png").await;
+    if bg_map.is_none() {
+        info!("couldnt load background");
+        return;
+    }
     let bg_map = bg_map.unwrap();
 
-    loop {
-        clear_background(BLUE);
+    //player
+    // let player = output::Player::initialize("").await;
+    // if player.is_none() {
+    //     info!("couldnt load player");
+    //     return;
+    // }
+    let mut mov = input::Movement::initialize(10.0);
 
-        bg_map.draw(&mut map_design);
+    loop {
+        clear_background(BLACK);
+
+        //input
+        input::Movement::read_and_set_vel(&mut mov);
+
+
+        //draw
+        bg_map.draw(0, 0);
+        output::Player::draw_temp();
 
         next_frame().await
     }
