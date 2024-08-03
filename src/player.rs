5be4ee843df3f1b9ffa60a2263535ceddeaf4d84
CyberAsh18 <ashfaaq.riphque@cyberowl.io>
2024-08-03 17:18:03 +0800

    added map boundary collision

--- src/player.rs
+++ src/player.rs
@@ -5,7 +5,7 @@ use crate::{custom::{Direction, Point}, equipment, input::Movement, BackgroundMa
 
 const WIDTH: f32 = 32.0;
 const HEIGHT: f32 = 32.0;
-
+const WORLDBITSIZE: f32 = 32.0/2.0;
 pub struct Player {
     //sprite_sheet: Texture2D,
     pub pos: Point,
@@ -58,21 +58,42 @@ impl Player {
         let screen_half_size_x = WINDOW_WIDTH/2.0 - self.size.x / 2.0;
         let screen_half_size_y = WINDOW_HEIGHT/2.0 - self.size.y / 2.0;
         
+        //camera
         if pos.x < screen_half_size_x + map.pos.x {
-            self.pos.x += vel.x;
+            if pos.x < WORLDBITSIZE {
+                //boundary
+                self.pos.x = WORLDBITSIZE;
+            }else {
+                self.pos.x += vel.x;
+            }
             map.pos.x = 0.0;
         } else if pos.x > screen_half_size_x + map.pos.x + map.background_img.width() - WINDOW_WIDTH {
-            self.pos.x += vel.x;
+            if pos.x > WINDOW_WIDTH - self.size.x - WORLDBITSIZE {
+                //boundary
+                self.pos.x = WINDOW_WIDTH - self.size.x - WORLDBITSIZE;
+            } else {
+                self.pos.x += vel.x;
+            }
         } else {
             map.pos.x -= vel.x;
             self.pos.x = screen_half_size_x;
         }
 
         if pos.y < screen_half_size_y + map.pos.y  {
-            self.pos.y += vel.y;
+            if pos.y < WORLDBITSIZE { 
+                //boundary
+                self.pos.y = WORLDBITSIZE;
+            } else {
+                self.pos.y += vel.y;
+            }
             map.pos.y = 0.0;
-        } else if pos.y > screen_half_size_y + map.pos.y + map.background_img.height() - screen_height() {
-            self.pos.y += vel.y;
+        } else if pos.y > screen_half_size_y + map.pos.y + map.background_img.height() - WINDOW_HEIGHT {
+            if pos.y > WINDOW_HEIGHT- self.size.y - WORLDBITSIZE {
+                //boundary
+                self.pos.y = WINDOW_HEIGHT - self.size.y - WORLDBITSIZE;
+            } else {
+                self.pos.y += vel.y;
+            }
         } else {
             map.pos.y -= vel.y;
             self.pos.y = screen_half_size_y;
